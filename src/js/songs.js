d19847238b97360f0d468753b9b9c744914f9906
Phil Sajdak <psajdak@myseneca.ca>
2023-12-05 16:07:32 -0500

    Moved files to restructure root

---
+++ src/js/songs.js
@@ -0,0 +1,271 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  // Artist 1
+  {
+    songId: "SID-001",
+    artistId: "AID-00001",
+    title: "Mysterons",
+    year: "1994",
+    duration: 301,
+    url: "https://youtu.be/GxWgRnnWfcc?si=dmarToZAQgd8srhF",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/3100/2617417404_fa7a5c4f4d_c.jpg"
+  },
+
+  {
+    songId: "SID-002",
+    artistId: "AID-00001",
+    title: "Sour Times",
+    year: "1994",
+    duration: 248,
+    url: "https://youtu.be/8B-i1vsA6jw?si=d2UkMuuWYsCKzjMt",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/8407044a456efc545ce74bde965a22f3.jpg#8407044a456efc545ce74bde965a22f3"
+  },
+
+  {
+    songId: "SID-003",
+    artistId: "AID-00001",
+    title: "Strangers",
+    year: "1994",
+    duration: 215,
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/dedfcc43acab45329c08bc1a5bf099f5.jpg#dedfcc43acab45329c08bc1a5bf099f5"
+  },
+
+  {
+    songId: "SID-004",
+    artistId: "AID-00001",
+    title: "It Could Be Sweet",
+    year: "1994",
+    duration: 250,
+    url: "https://youtu.be/o8MLqeReOmQ?si=1ivN9WXBxUbML0Uf",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/65a2c800f5cf57788f1916a1ca656e05.jpg#65a2c800f5cf57788f1916a1ca656e05"
+  },
+
+  {
+    songId: "SID-005",
+    artistId: "AID-00001",
+    title: "Wandering Star",
+    year: "1994",
+    duration: 273,
+    url: "https://youtu.be/o8MLqeReOmQ?si=1CXFi9rXq8rs0mMt",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/2df59483523d45c0bc3638269ccef781.jpg#2df59483523d45c0bc3638269ccef781"
+  },
+
+  {
+    songId: "SID-006",
+    artistId: "AID-00001",
+    title: "It's A Fire",
+    year: "1994",
+    duration: 230,
+    url: "https://youtu.be/tclxGyzv9t4?si=4CmKTruYnlLn45SU",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/3897/14707425664_31590ab7bd_c.jpg"
+  },
+
+  {
+    songId: "SID-007",
+    artistId: "AID-00001",
+    title: "Numb",
+    year: "1994",
+    duration: 213,
+    url: "https://youtu.be/DZFTDIGpyn0?si=MVHrKlIxWHRWjvbN",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/2919/14525892640_1b5242ff43_b.jpg"
+  },
+
+  {
+    songId: "SID-008",
+    artistId: "AID-00001",
+    title: "Roads",
+    year: "1994",
+    duration: 234,
+    url: "https://youtu.be/bTxIfAvg8VM?si=PvcV46Hitmur4Amb",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/a94386eab26e48fbb747b7d4500d07f2.jpg#a94386eab26e48fbb747b7d4500d07f2"
+  },
+
+  {
+    songId: "SID-009",
+    artistId: "AID-00001",
+    title: "Pedestal",
+    year: "1994",
+    duration: 204,
+    url: "https://youtu.be/69Lk08BgLkY?si=op7nBbnhpqqX5E1b",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/a367d3ef173f40ec85f87ec6d5e7580f.jpg#a367d3ef173f40ec85f87ec6d5e7580f"
+  },
+  // Artist 2
+  {
+    songId: "SID-0010",
+    artistId: "AID-00002",
+    title: "Angel",
+    year: "1998",
+    duration: 370,
+    url: "https://www.youtube.com/watch?v=frkOYc1eYWU",
+    explicit: false,
+    imageUrl: "https://i.scdn.co/image/52c2a824e84f4e8adf0b12418f9f8306b4b5b77a"
+  },
+
+  {
+    songId: "SID-0011",
+    artistId: "AID-00002",
+    title: "Risingson",
+    year: "1994",
+    duration: 274,
+    url: "https://www.youtube.com/watch?v=dxKQGn1C9Fc",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/bd73431c3e4747c09a421b30b9663bee.jpg#bd73431c3e4747c09a421b30b9663bee"
+  },
+
+  {
+    songId: "SID-0012",
+    artistId: "AID-00002",
+    title: "Teardrop",
+    year: "1994",
+    duration: 317,
+    url: "https://www.youtube.com/watch?v=DpUrH1mCAqQ",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/513525709958517ab0040cccea3edc33.jpg#513525709958517ab0040cccea3edc33"
+  },
+
+  {
+    songId: "SID-0013",
+    artistId: "AID-00002",
+    title: "Intertia Creeps",
+    year: "1998",
+    duration: 333,
+    url: "https://www.youtube.com/watch?v=H2EW40tSYCA",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/513525709958517ab0040cccea3edc33.jpg#513525709958517ab0040cccea3edc33"
+  },
+
+  {
+    songId: "SID-0014",
+    artistId: "AID-00002",
+    title: "Exchange",
+    year: "1994",
+    duration: 246,
+    url: "https://www.youtube.com/watch?v=bB-FJsg_LIM",
+    explicit: false,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/770x0/9bf8a999a2184faea48d54175b495b0a.jpg#9bf8a999a2184faea48d54175b495b0a"
+  },
+
+  {
+    songId: "SID-0015",
+    artistId: "AID-00002",
+    title: "Dissolved Girl",
+    year: "1994",
+    duration: 364,
+    url: "https://www.youtube.com/watch?v=t3705GOBsAQ",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/3909/14577811800_fcf72ccba4_b.jpg"
+  },
+
+  // Artist 3
+  {
+    songId: "SID-0016",
+    artistId: "AID-00003",
+    title: "Breath From Another",
+    year: "1998",
+    duration: 234,
+    url: "https://youtu.be/mFdGr5nh5tI?si=6zGNZjJYXR1y8QbI",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1238/535258820_40d35ceb36_b.jpg"
+  },
+
+  {
+    songId: "SID-0016",
+    artistId: "AID-00003",
+    title: "Heaven Sent",
+    year: "1998",
+    duration: 196,
+    url: "https://youtu.be/29AyODG_Jp0?si=l6-Ed4O4cDsCUejA",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1238/535258820_40d35ceb36_b.jpg"
+  },
+
+  {
+    songId: "SID-0018",
+    artistId: "AID-00003",
+    title: "Anywayz",
+    year: "1998",
+    duration: 210,
+    url: "https://youtu.be/XtclA9zH9gE?si=xsb4M-aWNEZmHq-1",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1178/535377017_ac52430764_b.jpg"
+  },
+
+  {
+    songId: "SID-0019",
+    artistId: "AID-00003",
+    title: "That Girl",
+    year: "1998",
+    duration: 191,
+    url: "https://youtu.be/wa6Zl9tHkeA?si=aDH712EC0denCgng",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1178/535377017_ac52430764_b.jpg"
+  },
+
+  {
+    songId: "SID-0020",
+    artistId: "AID-00003",
+    title: "Country Livin' (The World I Know)",
+    year: "1998",
+    duration: 210,
+    url: "https://youtu.be/JS8GMCus4gM?si=qCSmSBWzR0Or3y2f",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1117/535259160_30ce465d0a_b.jpg"
+  },
+
+  {
+    songId: "SID-0021",
+    artistId: "AID-00003",
+    title: "Flipher Oveture",
+    year: "1998",
+    duration: 42,
+    url: "https://youtu.be/QTlah1snRH0?si=m1lQRzxCBYEB-maI",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/1117/535259160_30ce465d0a_b.jpg"
+  },
+
+  {
+    songId: "SID-0022",
+    artistId: "AID-00003",
+    title: "Half A World Away",
+    year: "1998",
+    duration: 340,
+    url: "https://youtu.be/YNpWwckIV_E?si=0dG4_LpqsPxKUl__",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/38/78205405_2dedf616c4_c.jpg"
+  },
+
+  {
+    songId: "SID-0023",
+    artistId: "AID-00003",
+    title: "Lounge",
+    year: "1998",
+    duration: 350,
+    url: "https://youtu.be/zISO03BrfTc?si=Yb7x7FseXtvCyKeh",
+    explicit: false,
+    imageUrl: "http://live.staticflickr.com/38/78205405_2dedf616c4_c.jpg"
+  }
+];
